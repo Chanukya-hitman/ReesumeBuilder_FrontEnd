fa6d071fdf54e0be88530d3cbef23417191cf687
Chanukya-hitman <revathipathiyarabati@gmail.com>
2022-07-26 15:04:25 +0530

    Initial Commit

--- src/components/PersonalInfo.js
+++ src/components/PersonalInfo.js
@@ -1,4 +1,5 @@
 import { Form, Input } from 'antd'
+import TextArea from 'antd/lib/input/TextArea'
 import React from 'react'
 
 function PersonalInfo() {
@@ -6,10 +7,46 @@ function PersonalInfo() {
         <div>
             <div className="row">
                 <div className="col-md-4">
-                    <Form.Item name="firstName" label="FirstName">
+                    <Form.Item name="firstName" label="Firs tName" rules={[{ required: true }]}>
                         <Input />
                     </Form.Item>
                 </div>
+
+                <div className="col-md-4">
+                    <Form.Item name="lastName" label="Last Name" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                </div>
+
+                <div className="col-md-4">
+                    <Form.Item name="email" label="Email" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                </div>
+
+                <div className="col-md-4">
+                    <Form.Item name="mobileNumber" label="Mobile Number" rules={[{ required: true }]}>
+                        <Input />
+                    </Form.Item>
+                </div>
+
+                <div className="col-md-4">
+                    <Form.Item name="linkedIn" label="LinkedIn URL" rules={[{ required: false }]}>
+                        <Input />
+                    </Form.Item>
+                </div>
+                <div className="col-md-12">
+                <Form.Item name="objective" label="Objective" rules={[{ required: true }]}>
+                        <TextArea/>
+                    </Form.Item>
+                </div>
+
+                <div className="col-md-12">
+                <Form.Item name="address" label="Address" rules={[{ required: true }]}>
+                        <TextArea/>
+                    </Form.Item>
+                </div>
+
             </div>
         </div>
     )
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,29 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DefaultLayout from '../components/DefaultLayout'
 
-import { Form, Tabs } from 'antd';
+import { Button, Form, message, Spin, Tabs } from 'antd';
 import PersonalInfo from '../components/PersonalInfo';
+import SkillsEducation from '../components/SkillsEducation';
+import ExperienceProjects from '../components/ExperienceProjects';
+import axios from 'axios'
 
 
 const { TabPane } = Tabs;
 
 function Profile() {
+  const [loading,setLoading]=useState(false)
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+        const result = await axios.post('/api/users/update', values)
+        setLoading(false);
+        console.log(result);
+        message.success(result.data.message);
+    } catch (error) {
+        setLoading(false);
+        message.error('Registration failed')
+    }
+};
+
   return (
     <DefaultLayout>
+      {loading && <Spin size='large'></Spin>}
       <div className="update-profile">
         <h2>Update Profile</h2>
-        <Form layout="vertical">
-        <Tabs defaultActiveKey="1" >
-          <TabPane tab="Personal Info" key="1">
-            <PersonalInfo/>
-          </TabPane>
-          <TabPane tab="Skills and Education" key="2">
-            Content of Tab Pane 2
-          </TabPane>
-          <TabPane tab="Experience / Project" key="3">
-            Content of Tab Pane 3
-          </TabPane>
-        </Tabs>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Tabs defaultActiveKey="1" >
+            <TabPane tab="Personal Info" key="1">
+              <PersonalInfo />
+            </TabPane>
+            <TabPane tab="Skills and Education" key="2">
+              <SkillsEducation />
+            </TabPane>
+            <TabPane tab="Experience / Project" key="3">
+              <ExperienceProjects />
+            </TabPane>
+          </Tabs>
+          <Button htmlType="submit">UPDATE</Button>
         </Form>
       </div>
 
